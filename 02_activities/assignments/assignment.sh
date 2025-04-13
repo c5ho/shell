0aa48965cec985189538ac94a02529f9712e2f20
Calvin <c5ho@users.noreply.github.com>
2025-04-13 17:14:31 -0400

    Finished Part 1, Question 5

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -33,6 +33,7 @@ ls data/raw
 mkdir ./data/processed ./data/processed/server_logs ./data/processed/user_logs ./data/processed/event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
+cp data/raw/server*.log data/proccessed/server_logs
 
 # 6. Repeat the above step for user logs and event logs
 
